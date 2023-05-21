644a752b2f4b015223459ff8bf1b9d823304a2d8
nlprasanna5 <lakshmi.76740@gmail.com>
2023-05-21 17:57:37 +0530

    created counter application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import RandomQuoteGenerator from './components/quote/Quote';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleIncrementByTen = () => {
+    setCounter(counter + 10);
+  };
+
+  const handleDecrementByTen = () => {
+    setCounter(counter - 10);
+  };
+
   return (
-    <div className='container'>
-   <RandomQuoteGenerator/>
+    <div className="container">
+      <h1>Counter: {counter}</h1>
+      <div className="buttons-container">
+        <div className='up'>
+        <button className="button-up" onClick={handleIncrement}>Up</button>
+        </div>
+        <div className='right-left'>
+        <button className="button-left" onClick={handleDecrementByTen}>Left</button>
+        <button className="button-right" onClick={handleIncrementByTen}>Right</button>
+        </div>
+        <div className='down'>
+        <button className="button-down" onClick={handleDecrement}>Down</button>
+        </div>
+      </div>
     </div>
   );
 };
